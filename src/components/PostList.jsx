45e6a72ca722809d173537c05cbe15be9ec7ba8f
ayockson2500 <ayockson2500@gmail.com>
2022-09-20 10:04:07 +0100

    updated list post

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,29 +1,47 @@
-import React from "react";
-import { BiLike,  BiComment } from "react-icons/bi"
-import { FcLike } from "react-icons/fc"
-import "../css/PostList.css"
+import React, { useState } from "react";
+import { BiLike, BiComment, BiDotsHorizontalRounded } from "react-icons/bi";
+import { FcLike } from "react-icons/fc";
+import "../css/PostList.css";
 
-const PostList = ({ inputedValue, fileUpload }) => {
-  // const [listOfPost, setListOfPost] = useState([...inputedValue, inputedValue])
-  console.log(inputedValue);
+const PostList = ({ inputedValue, fileUpload, user }) => {
+  const [like, setLike] = useState(0)
 
+  const handleClickLike = () => {
+    setLike(like + 1)
+  }
+console.log(PostList);
   return (
     <div className="postList-Container">
       <div className="">
         {inputedValue.map((post, idx) => {
           return (
             <div key={idx} className="card mb-3 post-card">
+              <div className="d-flex justify-content-between p-2 user-header">
+                <div className="d-flex align-items-center">
+                  <img
+                    src={user.picture}
+                    alt={user.family_name}
+                    className="user-picture me-2"
+                  />
+                  <p className="text-center">{user.nickname}</p>
+                </div>
+                <div>
+                  <BiDotsHorizontalRounded />
+                </div>
+              </div>
               <div>{post.value}</div>
               <div>
-                <img src={fileUpload} alt="" />
+                <img src={fileUpload} alt="" className="upload-file" />
               </div>
               <div>
                 <div className="d-flex justify-content-between p-3 mb-2 borde-bt">
-                  <div><BiLike /> <FcLike /> 0 likes</div>
+                  <div>
+                    <BiLike /> <FcLike /> {like} likes
+                  </div>
                   <div>0 comments</div>
                 </div>
                 <div className="borde-bt">
-                  <button className="me-5 ms-2 btn">
+                  <button onClick={handleClickLike} className="me-5 ms-2 btn">
                     <BiLike /> Like
                   </button>
                   <button className="btn">
