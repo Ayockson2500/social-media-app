c5c977f20100e97a506b56fc3a6ea61a71dca7a9
ayockson2500 <ayockson2500@gmail.com>
2022-09-21 23:18:59 +0100

    updated list

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,15 +1,40 @@
 import React, { useState } from "react";
 import { BiLike, BiComment, BiDotsHorizontalRounded } from "react-icons/bi";
 import { FcLike } from "react-icons/fc";
+import { FaTrashAlt } from "react-icons/fa";
 import "../css/PostList.css";
+import Modal from "./Modal";
+import { Link } from "react-router-dom"
 
-const PostList = ({ inputedValue, fileUpload, user }) => {
-  const [like, setLike] = useState(0)
+const PostList = ({ inputedValue, user, setInputedValue }) => {
+  const [like, setLike] = useState(0);
+  const [show, setShow] = useState(false);
 
   const handleClickLike = () => {
-    setLike(like + 1)
-  }
-console.log(PostList);
+    setLike(like + 1);
+  };
+
+  const showModal = () => {
+    setShow(!show);
+  };
+
+  const handleDeletePost = (post) => {
+    const deletedPost = inputedValue.filter(
+      (removePost) => removePost.id !== post.id
+    );
+    setInputedValue(deletedPost);
+  };
+
+  const handleEditPost = (post) => {
+    const editedPost = inputedValue.map((newEditedPost) => {
+      if (newEditedPost.id === post.id) {
+        return { ...newEditedPost, isEditable: true };
+      }
+      return {...newEditedPost, isEditable: false}
+    });
+    console.log(editedPost);
+  };
+
   return (
     <div className="postList-Container">
       <div className="">
@@ -26,26 +51,38 @@ console.log(PostList);
                   <p className="text-center">{user.nickname}</p>
                 </div>
                 <div>
-                  <BiDotsHorizontalRounded />
+                  <BiDotsHorizontalRounded
+                    className="edit-button"
+                    onClick={() => handleEditPost(post)}
+                  />
+                  <Modal show={show} />
                 </div>
               </div>
               <div>{post.value}</div>
               <div>
-                <img src={fileUpload} alt="" className="upload-file" />
+                <img src={post.image} alt="" className="upload-file" />
               </div>
               <div>
                 <div className="d-flex justify-content-between p-3 mb-2 borde-bt">
                   <div>
                     <BiLike /> <FcLike /> {like} likes
                   </div>
-                  <div>0 comments</div>
+                  <div>
+                    <Link to="/comments">0 comments</Link>
+                    </div>
                 </div>
                 <div className="borde-bt">
                   <button onClick={handleClickLike} className="me-5 ms-2 btn">
                     <BiLike /> Like
                   </button>
-                  <button className="btn">
+                  <button className="btn me-5">
                     <BiComment /> Comment
+                  </button>
+                  <button
+                    className="btn"
+                    onClick={() => handleDeletePost(post)}
+                  >
+                    <FaTrashAlt /> Delete Post
                   </button>
                 </div>
               </div>
